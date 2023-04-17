e93ae64cf919d3a65174839501af84c92ca2c571
ADziakon <a.dziakanchuk@gmail.com>
2023-04-18 01:07:21 +0300

    feat: add tasks

--- src/mytest.js
+++ src/mytest.js
@@ -245,6 +245,28 @@
 //  let a= calculateDepth(arr);
 //  console.log();
 
+//sort by height
+
+// let arr = [-1, 150, 190, 170, -1, -1, 160, 180];
+// let arrNew = [];
+// for(let i=0;i<arr.length;i++){
+//   if(arr[i] != -1){
+//     arrNew.push(arr[i]);
+//     arr[i] = '';
+//   }
+// }
+// console.log("arr: " + arr);
+// console.log("arrNew: " + arrNew);
+// arrNew.sort((a,b) => { return a-b});
+// console.log("arrNew: " + arrNew);
+// for(let i=0;i<arr.length;i++){
+//   if(arr[i] == ''){
+//     arr[i] = arrNew[0];
+    
+//   }
+// }
+// return arr;
+
 
 
 //transform array
--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,25 +5,62 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    return this.chain.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
+  addLink(value) {
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    this.chain.push(value);
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
+  removeLink(position) {
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    if(position <= 0 || position > this.chain.length || typeof position != 'number'){
+      this.chain = [];
+      throw new Error('You can\'t remove incorrect link!'); 
+    }
+    
+    this.chain.splice(position-1,1);
+    return this;
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    let temp;
+    for(let i=0;i<Math.floor(this.chain.length/2);i++){
+      temp = this.chain[i];
+      this.chain[i] = this.chain[this.chain.length-i-1];
+      this.chain[this.chain.length-i-1] = temp;
+    }
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    let strEx = '( )';
+    let strStart = '( ';
+    let strEnd = ' )';
+    let strMiddle = ' )~~( ';
+    let str = '( ';
+    if(this.chain.length == 0){
+      return strEx;
+    }
+    for(let i=0;i<this.chain.length;i++){
+      if(i<this.chain.length-1){
+        str += this.chain[i] + strMiddle;
+      }else{
+        str += this.chain[i]+strEnd;
+      }
+      
+    }
+  this.chain = [];
+  return str;
+
   }
 };
 
